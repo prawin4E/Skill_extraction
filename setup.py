0176bee2aeaa03122bd1a1ce3ff4803a94159541
pravin kumar S <pravinkumar.s@itechindia.co>
2023-12-09 17:40:29 +0530

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-install_requires = [
-    "skillNer",
-    "spacy>=3.0.0,<4.0.0",
-    "en_core_web_lg",
-    "fastapi",
-    "uvicorn[standard]",
-    
-]
-
 setup(
-    install_requires=install_requires,
+    name='Itech Skills Extractor',
+    version='0.1',
+    py_modules=['skill_extractor'],
+    install_requires=[
+        "skillNer",
+        "spacy>=3.0.0,<4.0.0",
+        "en_core_web_lg",
+        "fastapi",
+        "uvicorn[standard]"
+    ]
 )
